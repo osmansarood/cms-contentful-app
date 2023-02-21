83c205981a5e7c3800e27bcf6ae2e93f342662c9
Osman Sarood <osmansarood@gmail.com>
2023-02-21 14:29:39 -0800

    Fetching from Valhalla for index page only -- there's a toggle use_val var inside getAllPostsForHome function in lib\api

--- components/date.js
+++ components/date.js
@@ -1,9 +1,16 @@
 import { format } from 'date-fns'
 
+// Gatsby converts the dateString to epoch string which isn't typical.
+// Can we store epoch as uint?
 export default function DateComponent({ dateString }) {
+  let dateStringFormatted = dateString
+  if (isNaN(dateString) == false) {
+    const epochTime = parseInt(dateString, 10);
+    dateStringFormatted = new Date(epochTime).toISOString();
+  }
   return (
-    <time dateTime={dateString}>
-      {format(new Date(dateString), 'LLLL	d, yyyy')}
+    <time dateTime={dateStringFormatted}>
+      {format(new Date(dateStringFormatted), 'LLLL	d, yyyy')}
     </time>
   )
 }
